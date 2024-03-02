12a6becf88f41ef578e46ba065f924f8b5e52f4b
Ionut Antoniu <ioni_gherlan@yahoo.com>
2024-03-02 20:12:46 +0200

    Added the functionalities

---
+++ script.js
@@ -0,0 +1,18 @@
+const scrollContainer = document.querySelector(".gallery");
+const backBtn = document.getElementById("back-btn");
+const nextBtn = document.getElementById("next-btn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+});
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 900;
+});
